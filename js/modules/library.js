15d261b4e26993c3ddb4fdd29109cfc3c21471dd
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2025-03-11 18:36:55 -0400

    Delegate some of bookEntry's duties to bookKey

--- js/modules/library.js
+++ js/modules/library.js
@@ -18,38 +18,66 @@ function Book(title, author, pages, isRead = false) {
 
 function bookEntry(book) {
   const bookDiv = document.createElement('div');
+  bookDiv.classList.add('book-entry');
 
-  bookDiv.classList.add('book-entry')
+  const linkDiv = document.createElement('div');
+  linkDiv.classList.add('card-link');
+  linkDiv.setAttribute('data-events', 'click-toggleIsRead')
   
   for (const [key, input] of Object.entries(book)) {
     console.log([key, input]);
-    if (key === 'isRead' && input.value === 'on') {
-      bookDiv.classList.add('is-read');
-      continue;
-    } 
-
-    if (key === 'id') {
+    switch (key) {
+      case 'id':
       bookDiv.setAttribute('id', input);
       continue;
+      case 'isRead':
+        if(input.checked) {bookDiv.classList.add('is-read');}
+        continue;
+      case 'title':
+        linkDiv.appendChild(bookKey(key, input.value));
+        break;
+      case 'author':
+        linkDiv.appendChild(bookKey(key, input.value));
+        break;
+      case 'pages':
+        linkDiv.appendChild(bookKey(key, `${input.value} pages`));
+        break;
     }
-
-    if (key === 'div' || key === 'info') {continue;}
-
-    const divEntry = document.createElement('div');
-    divEntry.classList.add(`book-${key}`);
-    const node = document.createTextNode(input.value);
-    divEntry.append(node);
-    bookDiv.append(divEntry);
   }
-
   const removeButton = document.createElement('button');
+  removeButton.textContent = 'Remove Book'
   removeButton.dataset.events = 'click-removeBook';
-  bookDiv.append(removeButton);
+
+  bookDiv.appendChild(linkDiv)
+  bookDiv.appendChild(removeButton);
 
   return bookDiv;
 }
 
+function bookKey (key, value) {
+  const [header, cell] = [document.createElement('div'), document.createElement('div')];
+  [header.textContent, cell.textContent] = [`${key.charAt(0).toUpperCase()}${key.slice(1)}:`,value]
+  header.classList.add('card-header');
+  cell.classList.add('card-cell');
 
-export { library, Book, bookEntry }
+  const divEntry = document.createElement('div');
+  divEntry.classList.add(`'book-${key}`);
+
+  divEntry.appendChild(header);
+  divEntry.appendChild(cell);
+
+  return divEntry;
+}
+
+const theHobbit = new Book(
+    'The Hobbit',
+    'J.R.R. Tolkien',
+    295,
+    false
+  )
+  
+  console.log(theHobbit.info())
+
+export { library, Book }
 
 
