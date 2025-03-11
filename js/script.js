6c683c1f77ff1ebb5c3d964feb48915d4387d769
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2025-03-11 18:38:33 -0400

    Move example book to library.js

--- js/script.js
+++ js/script.js
@@ -3,11 +3,3 @@ import * as eventFunctions from './modules/eventFunctions.js';
 eventFunctions.addEventListeners();
 
 
-// const theHobbit = new library.Book(
-//     'The Hobbit',
-//     'J.R.R. Tolkien',
-//     295,
-//     false
-//   )
-  
-//   console.log(theHobbit.info())
